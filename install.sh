2bc120c9be2cf30be87e69be9f06326f00a63576
Michael Myers <michael.myers@wustl.edu>
2026-01-07 19:27:54 -0600

    updated appearance of old pkg name etc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-REPO="myersm0/poker-terminal"
+REPO="myersm0/transparent-poker"
 
 curl --proto '=https' --tlsv1.2 -LsSf \
-    "https://github.com/${REPO}/releases/latest/download/poker-terminal-installer.sh" | sh
+    "https://github.com/${REPO}/releases/latest/download/transparent-poker-installer.sh" | sh
